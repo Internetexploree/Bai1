d135334fc543186c83335211cf5241192a8468e2
Internetexploree <lequi1322@gmail.com>
2023-03-24 00:08:51 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Moi nhap gia tri a: ");
+            int a = Int16.Parse(Console.ReadLine());
+
+            Console.Write("Moi nhap gia tri b: ");
+            int b = Int16.Parse(Console.ReadLine());
+
+            int tong = a + b;
+            int hieu = a - b;
+            int tich = a * b;
+            float thuong = (float)a / b;
+            Console.WriteLine("Tong cua a va b la: {0} ", +tong);
+            Console.WriteLine("Hieu cua a va b la: {0} ", +hieu);
+            Console.WriteLine("Tich cua a va b la: {0} ", +tich);
+            Console.WriteLine("Thuong cua a va b la: {0} ", +thuong);
+            Console.ReadLine();
+        }
+    }
+}
